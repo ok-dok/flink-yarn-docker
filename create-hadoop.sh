d8534c0bcbb93f1c1dfee028afddeecbeec29a75
Shawn Hou <shawn_hou@163.com>
2019-04-26 11:46:00 +0800

    增加创建集群的脚本

---
+++ create-hadoop.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+create_master() {
+    docker run -it --name hadoop-master --network hadoop --network-alias master -h master \
+            -p 50010:50010  -p 50020:50020      -p 50070:50070  -p 50075:50075  -p 50090:50090 \
+            -p 8020:8020    -p 9000:9000        -p 10020:10020  -p 8030:8030    -p 19888:19888 \
+            -p 8031:8031    -p 8032:8032        -p 8033:8033    -p 8040:8040    -p 8042:8042 \
+            -p 8088:8088    -p 49707:49707      -p 2122:2122    $1  -d
+}
+
+create_slave() {
+    docker run -dit --name "hadoop-slave$1" --network hadoop --network-alias "slave$1" -h "slave$1" \
+    -p 50010    -p 50020    -p 50070    -p 50075    -p 50090    -p 8020     -p 9000 \
+    -p 10020    -p 8030     -p 19888    -p 8031     -p 8032     -p 8033     -p 8040 \
+    -p 8042     -p 8088     -p 49707    -p 2122     $2
+}
+
+help() {
+    echo "Usage: `basename $0 ` OPTIONS"
+    echo "Options: "
+    echo -e "\t -m,\t--master \t\t Create and start a hadoop container as master"
+    echo -e "\t -s,\t--slave number\t\t Create and start a specified number containers of hadoop as slave"
+}
+if [[ $# -lt 2 ]]; then
+    help
+    exit 0
+elif [[ "${1:0:1}" = "-" ]]; then
+    if [[ "$1" = "-m" || "$1" = "--master" ]]; then
+        create_master $2
+    elif [[ "$1" = "-s" || "$1" = "--salve" ]]; then
+        if [ $# -eq 3 ]; then
+            for i in `seq $2`
+            do
+                create_slave $i $3
+            done
+        else
+            help
+            exit 0
+        fi
+    else
+        help
+        exit 0
+    fi
+else
+    help
+    exit 0
+fi
+
+
--- docker-build/docker-entrypoint.sh
+++ docker-build/docker-entrypoint.sh
@@ -2,15 +2,15 @@
 set -ex
 
 service ssh start
-echo N | hdfs namenode -format
+gosu hadoop echo N | exec gosu hadoop $HADOOP_HOME/bin/hdfs namenode -format >>/dev/null
 gosu hadoop $HADOOP_HOME/sbin/start-dfs.sh
 gosu hadoop $HADOOP_HOME/sbin/start-yarn.sh
 
 if [ "$1" = "-d" ]; then
-  tail -f $HADOOP_HOME/logs/*
+  tail -f $HADOOP_HOME/logs/hadoop-hadoop-datanode-*.log $HADOOP_HOME/logs/hadoop-hadoop-namenode-*.log 
 fi
 
-if [ "$1" == "-bash" ]; then
+if [ "$1" = "-bash" ]; then
   /bin/bash
 fi
 
